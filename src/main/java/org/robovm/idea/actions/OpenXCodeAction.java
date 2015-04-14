8bbb03fcb890286251874d2bb5deb30337afd2cb
badlogic <badlogicgames@gmail.com>
2015-04-14 09:15:59 +0200

    Closes #37, open xcode context menu

--- src/main/java/org/robovm/idea/actions/OpenXCodeAction.java
+++ src/main/java/org/robovm/idea/actions/OpenXCodeAction.java
@@ -16,35 +16,31 @@
  */
 package org.robovm.idea.actions;
 
-import com.intellij.ide.IdeView;
-import com.intellij.openapi.actionSystem.*;
-import com.intellij.openapi.compiler.CompileContext;
-import com.intellij.openapi.compiler.CompileScope;
-import com.intellij.openapi.compiler.CompileStatusNotification;
-import com.intellij.openapi.compiler.CompilerManager;
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.roots.ModuleRootManager;
 import com.intellij.openapi.ui.DialogWrapper;
 import com.intellij.openapi.vfs.VfsUtil;
 import com.intellij.openapi.vfs.VirtualFile;
-import com.intellij.openapi.vfs.VirtualFileSystem;
 import org.robovm.idea.RoboVmIcons;
 import org.robovm.idea.RoboVmPlugin;
 import org.robovm.idea.interfacebuilder.IBIntegratorManager;
 import org.robovm.idea.interfacebuilder.IBIntegratorProxy;
 
-import javax.sql.CommonDataSource;
 import java.io.File;
 
 /**
- * Created by badlogic on 08/04/15.
+ * Created by badlogic on 14/04/15.
  */
-public class NewStoryboardAction extends AnAction {
+public class OpenXCodeAction extends AnAction {
     private Module module;
 
-    public NewStoryboardAction() {
-        super("Storyboard", "An iOS Storyboard", RoboVmIcons.ROBOVM_SMALL);
+    public OpenXCodeAction() {
+        super("Open Xcode Project", "Opens the Xcode project of this RoboVM module", RoboVmIcons.ROBOVM_SMALL);
     }
 
     public void actionPerformed(AnActionEvent e) {
@@ -52,19 +48,11 @@ public class NewStoryboardAction extends AnAction {
             return;
         }
 
-        VirtualFile file = CommonDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
-        final NewStoryboardDialog dialog = new NewStoryboardDialog(e.getProject(), new File(file.getCanonicalPath()));
-        dialog.show();
-        if(dialog.getExitCode() == DialogWrapper.OK_EXIT_CODE) {
-            IBIntegratorProxy proxy = IBIntegratorManager.getInstance().getProxy(module);
-            if(proxy == null) {
-                RoboVmPlugin.logError("Couldn't get interface builder integrator for module %s", module.getName());
-            } else {
-                File resourceDir = new File(file.getCanonicalPath());
-                proxy.newIOSStoryboard(dialog.getStoryboardName(), resourceDir);
-                VirtualFile vsfFile = VfsUtil.findFileByIoFile(resourceDir, true);
-                vsfFile.refresh(false, true);
-            }
+        IBIntegratorProxy proxy = IBIntegratorManager.getInstance().getProxy(module);
+        if(proxy == null) {
+            RoboVmPlugin.logError("Couldn't get interface builder integrator for module %s", module.getName());
+        } else {
+            proxy.openProjectFile(RoboVmPlugin.getModuleInfoPlist(module).getAbsolutePath());
         }
     }
 
@@ -72,7 +60,12 @@ public class NewStoryboardAction extends AnAction {
     public void update(AnActionEvent e) {
         Presentation presentation = getTemplatePresentation();
         VirtualFile file = CommonDataKeys.VIRTUAL_FILE.getData(e.getDataContext());
-        if(file == null || !file.isDirectory() || (module = RoboVmPlugin.isRoboVmModuleResourcePath(file)) == null) {
+        for(Module module: ModuleManager.getInstance(e.getProject()).getModules()) {
+            if(ModuleRootManager.getInstance(module).getFileIndex().isInContent(file)) {
+                this.module = module;
+            }
+        }
+        if(module == null || !RoboVmPlugin.isRoboVmModule(module)) {
             presentation.setEnabled(false);
             presentation.setVisible(false);
             return;

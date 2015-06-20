c2499f5f0809e98f6ad94e42b267d5c08739b30e
badlogic <badlogicgames@gmail.com>
2015-06-20 15:17:46 +0200

    Optimized imports, not sure why build fails on jenkins, builds correctly locally

--- src/main/java/org/robovm/idea/components/RoboVmBuildProcessParametersProvider.java
+++ src/main/java/org/robovm/idea/components/RoboVmBuildProcessParametersProvider.java
@@ -1,21 +1,17 @@
 package org.robovm.idea.components;
 
-import com.google.gson.Gson;
-import com.intellij.compiler.server.BuildProcessParametersProvider;
-import com.intellij.openapi.externalSystem.util.ExternalSystemApiUtil;
-import com.intellij.openapi.module.Module;
-import com.intellij.openapi.module.ModuleManager;
-import com.intellij.util.PathUtil;
-import com.intellij.util.containers.ContainerUtil;
-import groovy.lang.GroovyObject;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.tools.ant.taskdefs.Ant;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.plugins.gradle.util.GradleConstants;
 import org.slf4j.Logger;
 import org.slf4j.impl.Log4jLoggerFactory;
 
-import java.util.ArrayList;
-import java.util.List;
+import com.google.gson.Gson;
+import com.intellij.compiler.server.BuildProcessParametersProvider;
+import com.intellij.util.PathUtil;
+import groovy.lang.GroovyObject;
 
 /**
  * Created by badlogic on 19/06/15.
